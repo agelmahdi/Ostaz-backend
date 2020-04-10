795cac258f26126bb5a16141e1163de78c70c53d
Ahmed.Tawfeek <ahmed.tawfeek@Andalusiagroup.net>
2020-04-11 01:34:40 +0200

    [feature]test without cors

--- routes/api.php
+++ routes/api.php
@@ -17,36 +17,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
 
-Route::group( ['middleware' => ['cors']], function () {
-    Route::get( '/', function (Request $request) {
-          return $request->input('name');
+//Route::group(['middleware' => ['cors']], function () {
+    Route::post('/', function (Request $request) {
+        return $request->get('name');
+    });
+//});
+//Route::group(['middleware' => ['cors']], function () {
+    Route::group(['prefix' => 'follower'], function () {
+        Route::post('login', 'Auth\ApiAuthFollowerController@authenticate');
+        Route::post('register', 'Auth\ApiAuthFollowerController@register');
+
+        Route::group(['middleware' => ['jwt.verify']], function () {
+            Route::post('update-profile-image', 'Auth\ApiAuthFollowerController@updateProfileImage');
+            Route::put('update-profile', 'Auth\ApiAuthFollowerController@updateProfileImage');
+            Route::put('update-profile-password', 'Auth\ApiAuthFollowerController@updateProfilePassword');
+            Route::get('me', 'Auth\ApiAuthFollowerController@getAuthenticatedUser');
         });
-});
-Route::group( [
-    'prefix' => 'follower','middleware' => ['cors']
-], function () {
-    Route::post( 'login', 'Auth\ApiAuthFollowerController@authenticate' );
-    Route::post( 'register', 'Auth\ApiAuthFollowerController@register' );
-
-    Route::group(['middleware' => ['jwt.verify']], function() {
-        Route::post( 'update-profile-image', 'Auth\ApiAuthFollowerController@updateProfileImage' );
-        Route::put( 'update-profile', 'Auth\ApiAuthFollowerController@updateProfileImage' );
-        Route::put( 'update-profile-password', 'Auth\ApiAuthFollowerController@updateProfilePassword' );
-        Route::get( 'me', 'Auth\ApiAuthFollowerController@getAuthenticatedUser' );
     });
+    Route::group(['prefix' => 'streamer'], function () {
+        Route::post('login', 'Auth\ApiAuthStreamerController@authenticate');
+        Route::post('register', 'Auth\ApiAuthStreamerController@register');
 
-} );
-Route::group( [
-    'prefix' => 'streamer','middleware' => ['cors']
-], function () {
-    Route::post( 'login', 'Auth\ApiAuthStreamerController@authenticate' );
-    Route::post( 'register', 'Auth\ApiAuthStreamerController@register' );
-
-    Route::group(['middleware' => ['jwt.verify']], function() {
-        Route::post( 'update-profile-image', 'Auth\ApiAuthStreamerController@updateProfileImage' );
-        Route::put( 'update-profile', 'Auth\ApiAuthStreamerController@updateProfileImage' );
-        Route::put( 'update-profile-password', 'Auth\ApiAuthStreamerController@updateProfilePassword' );
-        Route::get( 'me', 'Auth\ApiAuthStreamerController@getAuthenticatedUser' );
+        Route::group(['middleware' => ['jwt.verify']], function () {
+            Route::post('update-profile-image', 'Auth\ApiAuthStreamerController@updateProfileImage');
+            Route::put('update-profile', 'Auth\ApiAuthStreamerController@updateProfileImage');
+            Route::put('update-profile-password', 'Auth\ApiAuthStreamerController@updateProfilePassword');
+            Route::get('me', 'Auth\ApiAuthStreamerController@getAuthenticatedUser');
+        });
     });
-
-} );
+//});
