7558135cd21493c9578f8fca3359544348fb095e
Ahmed.Tawfeek <ahmed.tawfeek@Andalusiagroup.net>
2020-04-10 23:26:20 +0200

    [feature]test api cors with front post

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });*/
 
 Route::group( ['middleware' => ['cors']], function () {
-    Route::get( '/', function () {
+    Route::post( '/', function () {
           return ['data'=>'Yes'];
         });
 });
