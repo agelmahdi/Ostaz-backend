f40daaf28269987aaa1bb8b75240c455045a99b5
Ahmed.Tawfeek <ahmed.tawfeek@Andalusiagroup.net>
2020-04-11 02:57:06 +0200

    [feature]ADD Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,13 +16,13 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Sec-Fetch-Dest', 'empty')
-            ->header('Sec-Fetch-Mode', 'cors')
-            ->header('Sec-Fetch-Sit', 'cross-site')
-            ->header('Accept-Encoding', 'gzip, deflate, br')
+//            ->header('Sec-Fetch-Dest', 'empty')
+//            ->header('Sec-Fetch-Mode', 'cors')
+//            ->header('Sec-Fetch-Sit', 'cross-site')
+//            ->header('Accept-Encoding', 'gzip, deflate, br')
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers',' Origin, X-Requested-With, Content-Type, Accept');
+            ->header('Access-Control-Allow-Credentials', 'true');
+//            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//            ->header('Access-Control-Allow-Headers',' Origin, X-Requested-With, Content-Type, Accept');
     }
 }
